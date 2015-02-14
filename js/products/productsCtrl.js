6504b371d2360022c64c990c8a8f36f86f65ae83
Paige <pguthrie+git@jive.com>
2015-02-14 12:13:01 -0700

    store app

---
+++ js/products/productsCtrl.js
@@ -0,0 +1,9 @@
+var app = angular.module('miniRouting');
+
+app.controller('productsCtrl', function($scope, $routeParams, productsService){
+	if($routeParams.id === 'shoes'){
+		$scope.productData = productsService.shoeData;
+	} else if($routeParams.id === 'socks'){
+		$scope.productData = productsService.sockData;
+	}
+});
